714318708abbe25956d6d47885fd4b7697b6ed00
aashishbiradar <aashish.biradar@gmail.com>
2020-06-07 15:01:42 +0530

    enabled cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import routes from './routes/route';
 
 const port = 3000;
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
